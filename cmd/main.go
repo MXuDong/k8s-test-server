e1f8eec9fa6c4041807b7d1dd99aabc102090639
A-Donga <1586793553@qq.com>
2021-01-05 11:34:14 +0800

    update config info

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
-func main(){
-	fmt.Println("start")
+var Version = ""
+var BuildPlatform = ""
+var BuildStamp = ""
+
+func main() {
+	if len(os.Args) > 1 {
+		fmt.Println(Version)
+	}
+	fmt.Println("Complete")
+}
+
+func showVersion() {
+	fmt.Println("The version is :", Version)
+	fmt.Println("The build from :", BuildPlatform)
+	fmt.Println("The build stamp:", BuildStamp)
 }
