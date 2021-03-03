2c40716ba08fc5aa71f941aa419d8d3cd2640d52
MXuDong <2941884109@qq.com>
2021-03-03 16:57:48 +0800

    add run shell

---
+++ platform/istio/vm/pre-vm.sh
@@ -0,0 +1,9 @@
+# export the envs, the pre-vm should run in current thread, or source it.
+
+export VM_APP=""
+export VM_NAMESPACE=""
+export WORK_DIR=""
+export SERVICE_ACCOUNT=""
+export CLUSTER_NETWORK=""
+export VM_NETWORK=""
+export CLUSTER="Kubernetes"
